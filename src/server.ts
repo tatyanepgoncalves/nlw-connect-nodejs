ec4025cfc9225c94244a3742a7ec5ec76aa60759
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-03-03 16:47:26 -0300

    ✨ feat: function access link and error correction

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 import { env } from "./env";
+import { accessInviteLinkRoute } from "./routes/access-invite-link";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -33,6 +34,7 @@ app.register(fastifySwaggerUi, {
 });
 
 app.register(subscribeToEventRoute)
+app.register(accessInviteLinkRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server running!");
